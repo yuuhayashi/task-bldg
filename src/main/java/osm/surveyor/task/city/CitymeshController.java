f9a2c8ea0f4c1f5f286286c7df6a75b47e1a641e
haya4 <hayashi.yuu@gmail.com>
2022-08-11 10:55:45 +0900

    マッパーリストから担当タスクの一覧表示を可能にした

--- src/main/java/osm/surveyor/task/city/CitymeshController.java
+++ src/main/java/osm/surveyor/task/city/CitymeshController.java
@@ -2,10 +2,13 @@ package osm.surveyor.task.city;
 
 import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import osm.surveyor.task.city.model.City;
@@ -25,4 +28,15 @@ public class CitymeshController {
 		model.addAttribute("meshes", tasks);
 		return "meshes";
 	}
+	
+	@GetMapping("/usertask")
+	public String userTask(@AuthenticationPrincipal UserDetails user, Model model,
+			@RequestParam(name="user") String username)
+	{
+		List<Citymesh> meshes = meshRepository.serchByUser(username);
+        model.addAttribute("username", username);
+		model.addAttribute("meshes", meshes);
+		return "meshlist";
+	}
+	
 }
--- src/main/java/osm/surveyor/task/city/CitymeshRepository.java
+++ src/main/java/osm/surveyor/task/city/CitymeshRepository.java
@@ -3,6 +3,8 @@ package osm.surveyor.task.city;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import osm.surveyor.task.city.model.Citymesh;
 import osm.surveyor.task.city.model.CitymeshPK;
@@ -10,4 +12,7 @@ import osm.surveyor.task.city.model.CitymeshPK;
 public interface CitymeshRepository extends JpaRepository<Citymesh,CitymeshPK> {
 
 	List<Citymesh> findByCitycode(String citycode);
+	
+	@Query("SELECT m FROM Citymesh m WHERE m.username = :username OR m.validator = :username order by citycode,meshcode")
+	List<Citymesh> serchByUser(@Param("username")String username);
 }
