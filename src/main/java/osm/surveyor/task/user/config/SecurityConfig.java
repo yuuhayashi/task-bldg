90bed47930e7ae611ab840f29a93697d3df7ef25
haya4 <hayashi.yuu@gmail.com>
2023-04-05 07:55:37 +0900

    UPDATE : spring-boot-starter-parent : 2.6.14

--- src/main/java/osm/surveyor/task/user/config/SecurityConfig.java
+++ src/main/java/osm/surveyor/task/user/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeHttpRequests()
 		        // 「/login」と「/error」をアクセス可能にします
 		        .antMatchers("/login", "/error", "/register").permitAll()
 		        .anyRequest().authenticated()
