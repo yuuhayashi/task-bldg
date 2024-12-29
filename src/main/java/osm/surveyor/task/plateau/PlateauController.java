bf6282789bac8a1c262a4bec5e355d4a42994e48
haya4 <hayashi.yuu@gmail.com>
2024-12-29 10:50:30 +0900

    PleteauController の開発途中

--- src/main/java/osm/surveyor/task/plateau/PlateauController.java
+++ src/main/java/osm/surveyor/task/plateau/PlateauController.java
@@ -1,20 +1,47 @@
-package osm.surveyor.task.index;
+package osm.surveyor.task.plateau;
+
+import java.nio.file.*;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.PathResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class IndexController {
+public class PlateauController {
 	@Value("${task-bldg.osm-data.url}")
-	private String url;
+	private String url;		// task-bldg.osm-data.url=http://surveyor.mydns.jp/osm-data
 
-	@GetMapping("/index")
-	public String index() {
+	@Value("${task-bldg.plateau.dir}")
+	private String dir;		// task-bldg.plateau.dir=plateau-data
+
+	@GetMapping(value = "/plateau/{citycode}}")
+	public String getPlateau(@PathVariable("citycode") String citycode) throws Exception {
+		try {
+			Path base = Path.of(dir);
+			if (Files.exists(base)) {
+				Files.createDirectory(base);
+			}
+			Path city = Path.of(dir, citycode);
+			if (Files.exists(city)) {
+				Files.createDirectory(city);
+			}
+			return "OK";
+		}
+		catch (Exception e) {
+			return e.toString();
+		}
+
+		/*
+		Path path = Path.of(dir.toString(), fileName);
+		PathResource resource = new PathResource(path);
+		
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			System.out.println(String.format("INFO: httpGet(%s)", url));
@@ -30,5 +57,6 @@ public class IndexController {
 		catch (Exception e) {
 			return e.toString();
 		}
+		*/
 	}
 }
